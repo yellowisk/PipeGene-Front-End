aeea6951de6894cc68bd733c82f1d9c67498dff7
Vinicius Silva <vinicius.luiz19@gmail.com>
2021-06-28 01:02:58 -0300

    feat: add authorization token for requests

--- src/app/interceptors/httpRequest.interceptor.ts
+++ src/app/interceptors/httpRequest.interceptor.ts
@@ -8,6 +8,7 @@ import {
 import { Observable } from 'rxjs';
 import {catchError, map} from 'rxjs/operators';
 import { LoadingService } from '../services/loading.service';
+import { AuthService } from '../services/auth.service';
 
 /**
  * This class is for intercepting http requests. When a request starts, we set the loadingSub property
@@ -19,21 +20,30 @@ import { LoadingService } from '../services/loading.service';
 export class HttpRequestInterceptor implements HttpInterceptor {
 
   constructor(
-    private readonly loadingService: LoadingService
+    private readonly loadingService: LoadingService,
+    private readonly authService: AuthService
   ) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     this.loadingService.setLoading(true, request.url);
-    return next.handle(request)
+    const token = this.authService.getToken();
+
+    const req = token ? this.addAuthorizationHeaderAndClone(request, token) : request.clone();
+
+    return next.handle(req)
       .pipe(catchError((err) => {
-        this.loadingService.setLoading(false, request.url);
+        this.loadingService.setLoading(false, req.url);
         return err;
       }))
       .pipe(map<HttpEvent<any>, any>((evt: HttpEvent<any>) => {
         if (evt instanceof HttpResponse) {
-          this.loadingService.setLoading(false, request.url);
+          this.loadingService.setLoading(false, req.url);
         }
         return evt;
       }));
+  }
+
+  private addAuthorizationHeaderAndClone(request: HttpRequest<any>, token: string) {
+    return request.clone({ headers: request.headers.append('Authorization', token) });
   }
 }
